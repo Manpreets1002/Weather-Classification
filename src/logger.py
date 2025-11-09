bdc294c307db660c7456539f9d166d3a2e511ab7
Manpreets1002 <bhatia.manpreet1224@gmail.com>
2025-11-09 16:06:55 +0530

    logging and exception

--- src/logger.py
+++ src/logger.py
@@ -7,7 +7,7 @@ LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
 log_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(log_path,exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(log_path)
+LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
